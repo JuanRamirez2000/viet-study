39ffb1f5413c0ec6209ff3cf9bd726dd37f6b460
Juan Ramirez <juanpram2000@gmail.com>
2024-04-24 15:48:18 -0700

    remake: going back to basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Keyboard from "./_components/Keyboard";
-
 export default function Home() {
   return (
-    <main className="h-screen w-full flex flex-col items-center justify-center">
-      <Keyboard />
-    </main>
+    <main className="h-screen w-full flex flex-col items-center justify-center"></main>
   );
 }
